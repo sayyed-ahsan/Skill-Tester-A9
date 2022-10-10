e432bd5a654aaf1703d97b0e6a3c3433ad393294
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-10-11 01:38:42 +0600

    icon

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import './Quiz.css'
+import { BsFillEyeFill } from 'react-icons/bs';
 
 const Quiz = () => {
     const quiz = useLoaderData();
@@ -13,11 +15,17 @@ const Quiz = () => {
 
             {
                 qustions.map(qustion =>
-                    <div>
-                        <h3 className='text-center'> {qustion.question}</h3>
-                        <div>
 
+                    <div className='quiz-card'>
+                        <h3 className='text-center'> {qustion.question}</h3>
+                        <BsFillEyeFill></BsFillEyeFill>
+                        <div className='bg-warning text-center quz-answer'>
+                            <h3>{qustion.options[0]}</h3>
+                            <h3>{qustion.options[1]}</h3>
+                            <h3>{qustion.options[2]}</h3>
+                            <h3>{qustion.options[3]}</h3>
                         </div>
+
                     </div>
                 )
             }
